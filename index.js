0a09169b238001c53bfacfe51fd491a851b548f0
alexander sampson <a.sampson206@gmail.com>
2023-10-11 21:14:49 -0700

    heyyy

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimalsArray = farmAnimals.split(' ')
+const [moo, neigh, baa, oink, cluck] = farmAnimalsArray
+const [bessie, , dolly, babe, little] = farmAnimalsArray
+const [blackAndWhite, , black, pink, ] = farmAnimalsArray
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
+const colors = ['red', 'orange', 'yellow', 'green', 'blue',  'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+const [r, o, y, g, b, , v] = colors
+const [, , , , , indg, ] = colors
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -9,6 +15,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const { muppetName, color, song, job, partner } = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +31,7 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const { album: { theMuppetMovie: { song2, song4 } }, nestedJob, nestedPartner } = nestedMuppet;
 
 // Strings
 
